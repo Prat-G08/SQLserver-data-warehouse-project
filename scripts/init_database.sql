1c5e81ca6e3ffeae2b1c453343c7946bae00c0bb
Prat-G08 <pratyushgorai2006@gmail.com>
2025-12-04 02:13:45 +0800

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,29 @@
+/*
+===================================================================
+CREATING THE WAREHOUSE DATABASE
+===================================================================
+Script purpose:
+This script creates a new database in SQL server called "DataWarehouse". 
+The script also creates three schemas within the database: 
+"bronze", "silver" and "gold"
+*/
+
+USE master;
+GO 
+
+--Create the 'DataWarehouse' database
+CREATE DATABASE DateWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+--Creating the schemas
+CREATE SCHEMA bronze;
+GO 
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
