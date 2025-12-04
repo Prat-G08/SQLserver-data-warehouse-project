9baf510abd0c422cca74e7e3a66a4e8582ff388f
Prat-G08 <pratyushgorai2006@gmail.com>
2025-12-04 20:18:00 +0800

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -30,3 +30,30 @@ FROM(
 	FROM bronze.crm_cust_info
 	WHERE cst_id IS NOT NULL) sub
 WHERE data_time_rank = 1
+
+
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt)
+SELECT 
+	prd_id,
+	REPLACE(SUBSTRING(TRIM(prd_key), 1, 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key, 7, LEN(TRIM(prd_key))) AS prd_key,
+	prd_nm,
+	COALESCE(prd_cost, 0) AS prd_cost,
+	CASE UPPER(TRIM(prd_line))
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END AS prd_line,
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+	CAST(DATEADD(day, -1, LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)) AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info
