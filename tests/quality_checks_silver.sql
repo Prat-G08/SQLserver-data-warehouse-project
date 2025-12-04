8d5eadee51cda7388ef1f940ab1f0b42a8a477af
Prat-G08 <pratyushgorai2006@gmail.com>
2025-12-05 02:51:40 +0800

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -25,7 +25,6 @@ SELECT DISTINCT
 	cst_gender
 FROM silver.crm_cust_info
 
-
 /*
 ===================================================================
 crm_prd_info
@@ -65,3 +64,34 @@ SELECT
 	*
 FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt
+
+/*
+===================================================================
+crm_sales_details
+===================================================================
+*/
+
+--Check for invalid dates
+SELECT
+	sls_order_dt
+FROM silver.crm_sales_details
+WHERE sls_order_dt <= 0 OR LEN(sls_order_dt) != 8 OR sls_order_dt > 20500101 OR sls_order_dt < 19000101
+
+--Check for invalid date orders
+SELECT 
+	*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+--Check data consistensy between sales, quantinty and price 
+--> Sales = quantity * price
+--> Must not be NULL, zero or negative
+SELECT DISTINCT 
+	sls_sales,
+	sls_quantity,
+	sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
