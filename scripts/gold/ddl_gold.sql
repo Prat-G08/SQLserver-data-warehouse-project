72f1d7f121a8a0b3262a2784abe65befdc7a7af9
Pratyush Gorai <pratyushgorai2006@gmail.com>
2025-12-08 01:29:20 +0800

    Create ddl_gold.sql

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,53 @@
+CREATE VIEW gold.dim_customers AS 
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	ci.cst_marital_status AS martial_status,
+	CASE WHEN ci.cst_gender != 'n/a' THEN ci.cst_gender --	CRM is the master table for gender
+		 ELSE COALESCE(ca.gen, 'n/a')
+	END AS gender,
+	ci.cst_create_date AS create_date,
+	ca.bdate AS birthdate,
+	la.cntry AS country
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 AS la 
+ON ci.cst_key = la.cid
+
+
+CREATE VIEW gold.dim_products AS
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+	pn.prd_id AS product_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintenance,
+	pn.prd_cost AS product_cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info AS pn
+LEFT JOIN silver.erp_px_cat_g1v2 AS pc
+ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL --Only current information needed
+
+
+CREATE VIEW gold.dim_sales AS
+SELECT 
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_quantity,
+	sls_price
+FROM silver.crm_sales_details
